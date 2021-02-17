c56b126a26dcf46f25179786e3d9b9960204ef2b
angyice <leonardo_villa@studenti.naba.it>
2021-02-18 00:39:17 +0100

    Initial commit

---
+++ Assets/Scripts/EarthRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotator : MonoBehaviour
+{
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * 250f * Time.deltaTime;
+        float moveVertical = Input.GetAxis("Vertical") * 250f * Time.deltaTime;
+
+        transform.Rotate(moveVertical, -moveHorizontal,  0, Space.World);
+    }
+}
---
+++ Assets/Scripts/ScepterShooting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScepterShooting : MonoBehaviour
+{
+    private float damage = 10f;
+    private float range = 100f;
+
+    public Transform ShootingPoint;
+
+    void Shooting()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(ShootingPoint.transform.position, ShootingPoint.transform.forward, out hit, range ))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+
+}
