29373093ef574f0b5a47d963fbecd1bc9ddb50b5
Alessandro Mapelli <56259457+alemape99@users.noreply.github.com>
2021-02-19 20:01:05 +0100

    Spawnpoint and scepter damage

---
+++ Assets/Scripts/BaseSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseSpawner : MonoBehaviour
+{
+    private float nextSpawnTime;
+
+    [SerializeField]
+    public GameObject virusPrefab;
+
+    [SerializeField]
+    public float spawnDelay = 10;
+
+    void Update()
+    {
+        if (ShouldSpawn())
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        Instantiate(virusPrefab, transform.position, transform.rotation);
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time >= nextSpawnTime;
+    }
+}
